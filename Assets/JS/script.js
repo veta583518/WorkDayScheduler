8ec2b1bd73d753b85d54028af8d10f31293c2132
CoCo Chanel <veta583518@gmail.com>
2020-11-01 22:40:33 -0600

    Troubleshooting current day and hour display

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,21 +1,12 @@
-// // save button in modal was clicked
-// $("#task-form-modal .btn-save").click(function() {
-//     // get form values
-//     var taskText = $("#modalTaskDescription").val();
-//     var taskDate = $("#modalDueDate").val();
-  
-//     if (taskText && taskDate) {
-//       createTask(taskText, taskDate, "toDo");
-  
-//       // close modal
-//       $("#task-form-modal").modal("hide");
-  
-//       // save in tasks array
-//       tasks.toDo.push({
-//         text: taskText,
-//         date: taskDate
-//       });
-  
-//       saveTasks();
-//     }
-//   });
+// display current date and time
+var date = moment().format('dddd, MMMM Do hh:mm:ss a');
+$("#currentDay").append(date);
+console.log(date);
+// audit time blocks to code appropriately as past present, future
+    // add if statements to compare current hour with task hour
+// set interval to update every 3-5 min (run audit time at the end)
+
+// save task from planner to local storage
+
+// load task from local storage 
+
--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 ## Purpose 
 Create a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
+### References
+Mockup:
 
-![mock-up]
+![Challenge 5 Mockup](./assets/images/3rd-party-apis-mockup.gif)
 
-Link to deployed site 
+Link to deployed site: 
+
 [WorkDayScheduler](https://veta583518.github.io/WorkDayScheduler/)
 
 Link to github repository:
+
 [GitHub](https://github.com/veta583518/WorkDayScheduler)
